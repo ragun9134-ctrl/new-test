a6e43fdf6782d79f50bd8a295fe1d2c2f609c3cb
Vignesh <vigneshvicky8888vicky@gmail.com>
2026-06-11 00:30:08 +0530

    Add files via upload

---
+++ gavaskar_sir_test_1.py
@@ -0,0 +1,124 @@
+#addtions
+'''a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("add",a+b)
+#subtraction
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("sub",a-b)
+#multipication
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("Multipaction",a*b)
+#division
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("division",a/b)
+#modulus
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("modulus",a%b)
+#Floor divistion
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("modulus",a//b)
+#Exponentition
+a=int(input("Enter the value"))
+b=int(input("Enter the value"))
+print("modulus",a**b)
+#calculataions(squar)
+area_of_squar=int(input("ethachum kudu ya:"))
+print("area odayathu=",area_of_squar**2)
+print("perameter odayathu=",4*area_of_squar)
+#cacluter(circle)
+cricle=float(int(input("ethachum kudu ya:")))
+print("circle oda area=",cricle,3.14**2)
+print("perameter oda area=",2*3.14*cricle)
+#retangle
+length=int(input("enter the number:"))
+birth=int(input("enter the number:"))
+print("length=",2*length)
+print("brith=",4*length)
+#4
+a=int(input("ennter the number:"))
+b=int(input("enter the valu:"))
+c=int(input("ennter the number:"))
+avg=(a+b+c)/3
+print(avg)
+#5
+a=int(input("ennter the number:"))
+b=int(input("enter the valu:"))
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+#root
+num=int(input("enter the valu:"))
+print(num**2)
+#simple
+d=float(input("ethachum kudu ya:"))
+b=float(input("ethachum kudu ya:"))
+v=float(input("ethachum kudu ya:"))
+si=(d*b*v)/100
+print("ethan ya simple inters uhh=",si)
+#compond interst
+a=float(input("ethachum kudu ya:"))
+b=float(input("ethachum kudu ya:"))
+c=float(input("ethachum kudu ya:"))
+d=int(input("compound inerest ahh:"))
+amount=a*(1+b/(100*d))(d*c)
+ci=amount-a
+print("compond interestr=",ci)
+print("final amount=",amount)
+#athimetic
+x=10
+x+=5
+print(x)
+#
+x=10
+x-=5
+print(x)
+#
+x=10
+x*=5
+print(x)
+#
+x=10
+x/=5
+print(x)
+#
+x=10
+x%=5
+print(x)
+#
+x=10
+x**=5
+print(x)
+#swaping
+a=10
+b=5
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+#user name:
+user_name=input("una name enna nu sollu ya: ")
+password=int(input("pawoed sollu ya:"))
+if user_name=='vignesh' and password==12345:
+    print("nee pogalam pa")
+else:
+    print("thabi nilu pa")
+#or vachu
+user_name=input("una name enna nu sollu ya: ")
+password=int(input("pawoed sollu ya:"))
+if user_name=='vignesh' or password==12345:
+    print("nee pogalam pa")
+else:
+    print("thabi nilu pa")
+#cube
+num=int(input("ethachum sollu ya"))
+cube=num**3
+print(cube)'''
